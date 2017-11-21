d42868cacb2e7122fbef63e33aab5c4a339dd0ba
mairahs <Nael-2012>
2017-11-21 15:42:23 +0100

    Finalisation recherche d'un joueur via son numéro de carte

--- src/AdminBundle/Controller/UserController.php
+++ src/AdminBundle/Controller/UserController.php
@@ -5,9 +5,9 @@ namespace AdminBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends Controller
 {
@@ -21,11 +21,27 @@ class UserController extends Controller
         if ($request->getMethod() === "POST" && !is_null($numberCard) && $numberCard !== "")
         {
             $player = $this->getDoctrine()->getRepository('AppBundle:Player')->findPlayerByNumberCard($numberCard);
-            if(!$player)
-            {
-                throw new AccessDeniedException('Aucun joueur ne possède ce numéro de carte');
+
+
+            if (null === $player) {
+                throw new NotFoundHttpException("Aucun joueur ne possède ce numéro de carte");
             }
+
+            return  $this->redirectToRoute('admin_displayplayer',['id'=>$player->getId()]);
         }
-            return  $this->render('@Admin/user/find_player.html.twig');
+
+        return $this->render('@Admin/user/find_player.html.twig');
+
+    }
+
+    public function displayAction($id)
+    {
+        $player = $this->getDoctrine()->getRepository('AppBundle:Player')->find($id);
+
+        if ( null === $player) {
+            throw new NotFoundHttpException("Aucun joueur ne possède ce numéro de carte");
+        }
+
+        return $this->render('@Admin/user/display_player.html.twig',['player'=>$player]);
     }
 }
