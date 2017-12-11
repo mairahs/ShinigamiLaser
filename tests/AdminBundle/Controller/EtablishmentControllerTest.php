80b4c78d97f7f4187911b1135bdcc50627fbfd4c
CaptLabel <tonio_fr@hotmail.com>
2017-12-11 10:48:42 +0100

    Testing

--- tests/AdminBundle/Controller/EtablishmentControllerTest.php
+++ tests/AdminBundle/Controller/EtablishmentControllerTest.php
@@ -20,25 +20,27 @@ class EtablishmentControllerTest extends WebTestCase
 
     public function testIndexEtablishment()
     {
+        $etablishments = $this->client->getContainer()->get('doctrine')->getRepository('AppBundle:Etablishment')->findBy([],[],1);
+
         $crawler = $this->client->request('GET', 'etablishments');
 
         $this->assertSame(1, $crawler->filter('html:contains("Liste des établissements ShinigamiLaser")')->count());
         $this->assertSame(1, $crawler->filter('html:contains(" Retour à l\'interface d\'administration")')->count());
-        $this->assertSame('ShinigamiLaser - ETABLISHMENT1', $crawler->filter('td')->eq(0)->text());
-        $this->assertSame('123', $crawler->filter('td')->eq(1)->text());
-        $this->assertSame('ville', $crawler->filter('td')->eq(2)->text());
+        $this->assertSame('ShinigamiLaser - '.strtoupper($etablishments[0]->getName()), $crawler->filter('td')->eq(0)->text());
+        $this->assertSame(strval($etablishments[0]->getCode()), $crawler->filter('td')->eq(1)->text());
+        $this->assertSame($etablishments[0]->getCity(), $crawler->filter('td')->eq(2)->text());
     }
 
     public function testShowEtablishment()
     {
-        $crawler = $this->client->request('GET', 'etablishment/1');
+        $etablishments = $this->client->getContainer()->get('doctrine')->getRepository('AppBundle:Etablishment')->findBy([],[],1);
 
-        $this->assertSame('SL - etablishment1', $crawler->filter('h4.card-title')->eq(0)->text());
+        $crawler = $this->client->request('GET', 'etablishment/'.$etablishments[0]->getId());
+
+        $this->assertSame('SL - '.$etablishments[0]->getName(), $crawler->filter('h4.card-title')->eq(0)->text());
         $this->assertSame(1, $crawler->filter('html:contains("Retour vers la liste des établissements")')->count());
-        $this->assertSame('ville', $crawler->filter('h4.card-title')->eq(1)->text());
-        $this->assertSame('123', $crawler->filter('h4.card-title')->eq(2)->text());
-
-
+        $this->assertSame($etablishments[0]->getCity(), $crawler->filter('h4.card-title')->eq(1)->text());
+        $this->assertSame(strval($etablishments[0]->getCode()), $crawler->filter('h4.card-title')->eq(2)->text());
     }
     protected function tearDown()
     {
